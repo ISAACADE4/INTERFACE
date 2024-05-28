33e8795f28f4ee27c8320e99a5fac68a5d237e17
ᗷOᒍᗩᑕK <129832208+ISAACADE4@users.noreply.github.com>
2024-05-28 14:27:44 +0100

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,21 @@
+public class Main {
+
+    @Override
+    public String toString() {
+        return "main{}";
+    }
+    static void addNumbers(int a, int b){
+        System.out.println(a+b);
+    }
+
+
+    public static void main(String[] args) {
+
+        Computer hpPavilionComputer = new Computer(new Storage(), new Processor(), new RAM());
+        System.out.println(hpPavilionComputer);
+
+        addNumbers(6,7);
+    }
+
+
+}
---
+++ RAM.java
@@ -0,0 +1,14 @@
+public class RAM {
+    String name ="DDR4";
+    int readSpeed= 3;
+    float writeSpeed=1.5F;
+
+    @Override
+    public String toString() {
+        return "RAM{" +
+                "name='" + name + '\'' +
+                ", readspeed=" + readSpeed +
+                ", writeSpeed=" + writeSpeed +
+                '}';
+    }
+}
